45413cd49be3859908c9bf68ea168af84856318f
Randy Oest <oest@amazingrando.com>
2025-10-06 17:43:42 -0400

    wip

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { initialProfile } from '@/lib/initial-profile'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+
+const SetupPage = async () => {
+  const profile = await initialProfile()
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileID: profile.id,
+        },
+      },
+    },
+  })
+
+  if (server) {
+    return redirect(`/servers/${server.id}`)
+  }
+
+  return (
+    <div>Create a server</div>
+  )
+}
+
+export default SetupPage
