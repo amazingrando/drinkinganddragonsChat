8427500ba4292db90cb47ed1bf80d2f3a1828d6f
Randy Oest <oest@amazingrando.com>
2025-10-18 15:06:59 -0400

    wip

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile"
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
 import ChatHeader from "@/components/chat/chat-header"
+import { ChatInput } from "@/components/chat/chat-input"
 
 interface ChannelIdPageProps {
   params: Promise<{ serverId: string, channelId: string }>
@@ -35,6 +36,10 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader serverId={channel.serverID} name={channel.name} type="channel" />
+      <div className="flex-1">
+        Future messages
+      </div>
+      <ChatInput apiUrl="/api/socket/messages" query={{ channelId: channel.id, serverId: channel.serverID }} name={channel.name} type="channel" />
     </div>
   )
 }
