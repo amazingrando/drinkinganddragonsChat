8f1624b7b1fe048144389b4df35e91d5507dd83e
Randy Oest <oest@amazingrando.com>
2025-10-23 11:58:44 -0400

    auth phase 3

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { updateSession } from '@/lib/supabase/middleware'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
-})
+export default async function middleware(request: any) {
+  return await updateSession(request)
+}
 
 export const config = {
   matcher: [
