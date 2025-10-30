f3ddc81c2e3c9249c6787c8e8eb6339846bb6b95
Randy Oest <oest@amazingrando.com>
2025-10-30 17:15:46 -0400

    pass user email into avatar

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -31,7 +31,7 @@ export const ServerMember = ({ member }: ServerMemberProps) => {
     <button onClick={onClick} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-muted/60 transition mb-1",
       params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700",
     )}>
-      <UserAvatar src={member.profile.imageUrl} className="h-8 w-8 md:h-8 md:w-8" />
+      <UserAvatar src={member.profile.email} className="h-8 w-8 md:h-8 md:w-8" />
       <p className={cn("font-semibold text-sm whitespace-nowrap text-ellipsis max-w-full overflow-hidden transition",
         params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white",
       )}>
