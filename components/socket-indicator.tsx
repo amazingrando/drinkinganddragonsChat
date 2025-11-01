9aec40d2062de63c4951dd72a05d29af55525a74
Randy Oest <oest@amazingrando.com>
2025-10-31 22:40:06 -0400

    socket indicator

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -8,15 +8,23 @@ export const SocketIndicator = () => {
 
   if (!isConnected) {
     return (
-      <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-        Realtime Not Connected
-      </Badge>
+      <div className="flex justify-center items-center h-screen" aria-description="Realtime Not Connected">
+        <div className="relative inline-flex">
+          <div className="w-4 h-4 bg-yellow-500 rounded-full"></div>
+          <div className="w-4 h-4 bg-yellow-500 rounded-full absolute top-0 left-0 motion-safe:animate-ping"></div>
+          <div className="w-4 h-4 bg-yellow-500 rounded-full absolute top-0 left-0 motion-safe:animate-pulse"></div>
+        </div>
+      </div>
     )
   }
 
   return (
-    <Badge variant="outline" className="bg-emerald-600 text-white border-none">
-      Realtime Connected
-    </Badge>
+    <div className="flex justify-center items-center h-screen" aria-description="Realtime Connected">
+      <div className="relative inline-flex">
+        <div className="w-4 h-4 bg-green-500 rounded-full"></div>
+        <div className="w-4 h-4 bg-green-500 rounded-full absolute top-0 left-0 motion-safe:animate-socket-ping"></div>
+        <div className="w-4 h-4 bg-green-500 rounded-full absolute top-0 left-0 motion-safe:animate-socket-pulse"></div>
+      </div>
+    </div>
   )
 }
