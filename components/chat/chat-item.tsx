2e9ef21d7709a1497b2bd2c0b44d9468da3a5cc7
Randy Oest <oest@amazingrando.com>
2025-11-10 15:20:54 -0500

    wip

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -102,12 +102,6 @@ export const ChatItem = ({ id, content, member, timestamp, fileUrl, deleted, cur
     });
   }, [content, form]);
 
-
-  // const isAdmin = member.role === MemberRole.ADMIN;
-  // const isModerator = member.role === MemberRole.MODERATOR;
-  // const isOwner = currentMember.id === member.id;
-  // const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
-  // const canEditMessage = !deleted && isOwner && !fileUrl;
   const viewerIsAdmin = currentMember.role === MemberRole.ADMIN;
   const viewerIsModerator = currentMember.role === MemberRole.MODERATOR;
   const isOwner = currentMember.id === member.id;
